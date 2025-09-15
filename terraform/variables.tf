ae201139a2ca3de4e951720d411b96e0c7c3793d
vijaygit999 <127684730+vijaygit999@users.noreply.github.com>
2025-09-15 10:39:50 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
